ad0ddc328c29696b0a116c8b7b423becd21114ec
Geert Roumen <geert.roumen@gmail.com>
2017-08-17 17:32:16 +0200

    Added speed support for the esp32 chip

--- HX711.cpp
+++ HX711.cpp
@@ -1,6 +1,26 @@
 #include <Arduino.h>
 #include <HX711.h>
+#ifdef ESP_H
+uint8_t shiftInSlow(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder) {
+    uint8_t value = 0;
+    uint8_t i;
 
+    for(i = 0; i < 8; ++i) {
+        digitalWrite(clockPin, HIGH);
+        delayMicroseconds(1);
+        if(bitOrder == LSBFIRST)
+            value |= digitalRead(dataPin) << i;
+        else
+            value |= digitalRead(dataPin) << (7 - i);
+        digitalWrite(clockPin, LOW);
+        delayMicroseconds(1);
+    }
+    return value;
+}
+#define SHIFTIN_WITH_SPEED_SUPPORT(data,clock,order) shiftInSlow(data,clock,order)
+#else
+#define SHIFTIN_WITH_SPEED_SUPPORT(data,clock,order) shiftIn(data,clock,order)
+#endif
 #if ARDUINO_VERSION <= 106
     // "yield" is not implemented as noop in older Arduino Core releases, so let's define it.
     // See also: https://stackoverflow.com/questions/34497758/what-is-the-secret-of-the-arduino-yieldfunction/34498165#34498165
@@ -60,14 +80,20 @@ long HX711::read() {
 	uint8_t filler = 0x00;
 
 	// pulse the clock pin 24 times to read the data
-	data[2] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-	data[1] = shiftIn(DOUT, PD_SCK, MSBFIRST);
-	data[0] = shiftIn(DOUT, PD_SCK, MSBFIRST);
+	data[2] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT, PD_SCK, MSBFIRST);
+	data[1] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT, PD_SCK, MSBFIRST);
+	data[0] = SHIFTIN_WITH_SPEED_SUPPORT(DOUT, PD_SCK, MSBFIRST);
 
 	// set the channel and the gain factor for the next reading using the clock pin
 	for (unsigned int i = 0; i < GAIN; i++) {
 		digitalWrite(PD_SCK, HIGH);
+		#ifdef ESP_H
+		delayMicroseconds(1);
+		#endif
 		digitalWrite(PD_SCK, LOW);
+		#ifdef ESP_H
+		delayMicroseconds(1);
+		#endif
 	}
 
 	// Replicate the most significant bit to pad out a 32-bit signed integer
